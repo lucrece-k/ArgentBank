c64519a7ac76c0ad9c168597a7442a6fee0922aa
kouame lucrèce <kouamelucrece@gmail.com>
2025-01-02 19:50:16 +0100

    updade page user et component Header

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,14 +1,26 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import logo from "../../assets/images/argentBankLogo.webp";
 import "./style.scss";
 function Header() {
+  const location = useLocation();
   return (
     <header className="Header">
       <img src={logo} />
-      <nav>
-        <i className="fa-solid fa-circle-user"></i>
-        <Link to={"/SignIn"}>Sign in</Link>
-      </nav>
+
+      {location.pathname === "/" || location.pathname === "/SignIn" ? (
+        <nav>
+          {" "}
+          <i className="fa-solid fa-circle-user"></i>
+          <Link to={"/SignIn"}>Sign in</Link>{" "}
+        </nav>
+      ) : (
+        <nav>
+          {" "}
+          <i className="fa-solid fa-circle-user"></i> nom
+          <i className="fas fa-sign-out"></i>
+          <Link to={"/"}>Sign out</Link>{" "}
+        </nav>
+      )}
     </header>
   );
 }
--- frontend/src/pages/User/index.tsx
+++ frontend/src/pages/User/index.tsx
@@ -1,15 +1,46 @@
+import { useEffect, useState } from "react";
 import Account from "../../components/Account";
 import "./style.scss";
 function User() {
+  const [showsection, setShowsection] = useState(true);
+  useEffect(() => {}, [showsection]);
+
   return (
     <div className="User">
-      <section className="header">
-        <h1>
-          Welcome back <br />
-          Prenom Nom!
-        </h1>
-        <button>Edit Name</button>
-      </section>
+      {showsection && (
+        <section className="header">
+          <h1>
+            Welcome back <br />
+            Prenom Nom!
+          </h1>
+          <button onClick={() => setShowsection(!showsection)}>
+            Edit Name
+          </button>
+        </section>
+      )}
+      {!showsection && (
+        <section className="user-info">
+          <p>Edit user info</p>
+          <form action="">
+            <div>
+              <label htmlFor="user-name">User name:</label>
+              <input type="text" id="user-name" name="user-name" />
+            </div>
+            <div>
+              <label htmlFor="first-name">First name:</label>
+              <input type="text" id="first-name" name="first-name" />
+            </div>
+            <div>
+              <label htmlFor="last name">Last name:</label>
+              <input type="text" id="last name" name="last name" />
+            </div>
+            <div className="save-cancel-button">
+              <button>Save</button>
+              <button>Cancel</button>
+            </div>
+          </form>
+        </section>
+      )}
       <Account />
       <Account />
       <Account />
